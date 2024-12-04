1e11d588a0edc1857486a2e78c60c26a0b6a720b
hanmpark <hanmin.01.park@gmail.com>
2024-12-04 18:46:35 +0100

    day04: refactorized code

--- day04/part2/main.py
+++ day04/part2/main.py
@@ -2,15 +2,10 @@ def main():
 	def get_strings(content, i, j):
 		string1, string2 = "", ""
 
-		print(f"i: {i}, j: {j}")
-		print(f"content[i][j]: {content[i][j]}")
 		for k in range(3):
 			string1 += content[i + k][j + k]
 		for k in range(3):
 			string2 += content[i + 2 - k][j + k]
-		if i == 138 and j == 0:
-			print(f"String 1: {string1}")
-			print(f"String 2: {string2}")
 		return string1, string2
 
 	count = 0
@@ -21,6 +16,8 @@ def main():
 
 		for i in range(1, rows - 1):
 			for j in range(1, cols - 1):
+				if content[i][j] != "A":
+					continue
 				string1, string2 = get_strings(content, i - 1, j - 1)
 				count += (string1 == "MAS" or string1 == "SAM") and (string2 == "MAS" or string2 == "SAM")
 
